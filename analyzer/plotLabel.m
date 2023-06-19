4a6979a66609fac60460313daee2be4635cd5e8d
wyqy <youjingze@msn.cn>
2023-06-20 00:05:50 +0800

    delete unused code

--- analyzer/plotLabel.m
+++
@@ -1,6 +0,0 @@
-function plotLabel(xtexts, ytexts, font_size)
-%PLOTLABEL 标签绘制
-
-    xlabel(xtexts, 'FontName', 'Arial', 'FontSize', font_size, 'FontWeight', 'bold');
-    ylabel(ytexts, 'FontName', 'Arial', 'FontSize', font_size, 'FontWeight', 'bold');
-end
--- analyzer/plotLegend.m
+++
@@ -1,9 +0,0 @@
-function plotLegend(texts, font_size, line_width, location)
-%PLOTLEGEND 图例绘制
-
-    [lgd, lgdobj, ~, ~] = legend(texts, 'FontName', 'Arial', 'FontSize', font_size, 'FontWeight', 'bold');
-    lgd.Location = location; lgd.Box = 'off';
-    child_line = findobj(lgdobj, 'type', 'Line');
-    set(child_line, 'LineWidth', line_width);
-end
-
